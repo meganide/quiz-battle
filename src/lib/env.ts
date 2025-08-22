2503c554de8aa5f3925a50a310b110929ef37552
meganide <renashassan@hotmail.com>
2025-08-22 15:59:58 +0200

    feat: env

---
+++ src/lib/env.ts
@@ -0,0 +1,34 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  /*
+   * Serverside Environment variables, not available on the client.
+   * Will throw if you access these variables on the client.
+   */
+  server: {
+    CONVEX_DEPLOYMENT: z.string().min(1),
+    CONVEX_URL: z.string().min(1),
+  },
+  /*
+   * Environment variables available on the client (and server).
+   *
+   * 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+   */
+  client: {
+    NEXT_PUBLIC_CONVEX_URL: z.string().min(1),
+  },
+  /*
+   * Specify what values should be validated by your schemas above.
+   * 
+   * If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
+   * For Next.js >= 13.4.4, you can use the experimental__runtimeEnv option and
+   * only specify client-side variables.
+   */
+  runtimeEnv: {
+    CONVEX_DEPLOYMENT: process.env.CONVEX_DEPLOYMENT,
+    CONVEX_URL: process.env.CONVEX_URL,
+    NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
+  },
+
+});
