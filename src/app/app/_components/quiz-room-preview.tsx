50e0a6c111ea1ca8e7dc99778b54559f1496c463
meganide <renashassan@hotmail.com>
2025-08-23 21:38:55 +0200

    style: shit

--- src/app/app/_components/quiz-room-preview.tsx
+++ src/app/app/_components/quiz-room-preview.tsx
@@ -1,4 +1,8 @@
+import { Eye } from "lucide-react"
+
 import type { Doc } from "~/convex/_generated/dataModel"
+
+import { Button } from "@/components/ui/button"
 
 type QuizRoomPreviewProps = {
   room: Doc<"rooms">
@@ -6,13 +10,20 @@ type QuizRoomPreviewProps = {
 
 export function QuizRoomPreview({ room }: QuizRoomPreviewProps) {
   return (
-    <article className="rounded-sm bg-gradient-to-b from-neutral-400 to-neutral-500 px-3 py-6">
+    <article className="flex items-center justify-between gap-4 rounded-sm bg-gradient-to-b from-neutral-400 to-neutral-500 px-3 py-6">
       <header className="flex flex-col">
         <span className="text-neutral-050 font-bold">{room.name}</span>
         <span className="text-neutral-050 font-semibold">
           {room.playerIds.length} players
         </span>
       </header>
+
+      <aside className="flex items-center gap-2">
+        <Button variant="secondary">
+          <Eye className="size-5" />
+        </Button>
+        <Button>Join</Button>
+      </aside>
     </article>
   )
 }
