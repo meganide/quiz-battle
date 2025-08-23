ff8a31b19e40ced88859cf072d587f61eb71408a
meganide <renashassan@hotmail.com>
2025-08-23 12:46:14 +0200

    feat: google auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import { useQuery } from "convex/react";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { useConvexAuth, useQuery } from "convex/react";
+import { Button } from "@/components/ui/button";
 import { api } from "../../convex/_generated/api";
+import { SignIn } from "./_components/sign-in";
 
 export default function Home() {
   const tasks = useQuery(api.tasks.get);
-  
+
+  const { isAuthenticated } = useConvexAuth();
+  const { signOut } = useAuthActions();
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+      {isAuthenticated ? <Button onClick={() => void signOut()}>Sign out</Button> : <SignIn />}
+      
+      {tasks?.map(({ _id, text }) => (
+        <div key={_id}>{text}</div>
+      ))}
     </main>
   );
 }
