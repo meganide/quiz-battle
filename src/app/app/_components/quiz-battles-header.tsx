67bfe0f0636328a47c40371fe46a68332c5d267f
meganide <renashassan@hotmail.com>
2025-08-24 22:10:35 +0200

    style: make quiz battle header mobile fridnly

--- src/app/app/_components/quiz-battles-header.tsx
+++ src/app/app/_components/quiz-battles-header.tsx
@@ -11,16 +11,16 @@ export function QuizBattlesHeader() {
   const roomsInfo = useQuery(api.rooms.queries.list)
 
   return (
-    <section className="flex w-full items-center justify-between">
-      <aside>
-        <article className="flex items-center gap-4 rounded-sm bg-neutral-500 px-3 py-2">
+    <section className="flex w-full flex-col gap-4 md:flex-row md:items-center md:justify-between md:gap-0">
+      <aside className="order-2 md:order-1">
+        <article className="flex items-center gap-2 rounded-sm bg-neutral-500 px-3 py-2 text-sm md:gap-4 md:text-base">
           <span>Live Games</span>
           <span className="text-neutral-050 font-bold">
             {roomsInfo?.numberOfActiveRooms ?? 0}
           </span>
         </article>
       </aside>
-      <div className="flex items-center gap-4">
+      <div className="order-1 flex flex-col gap-2 md:order-2 md:flex-row md:items-center md:gap-4">
         <JoinRoomDialog />
         <CreateRoomDialog />
       </div>
