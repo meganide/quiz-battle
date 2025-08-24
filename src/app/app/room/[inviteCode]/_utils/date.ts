1b36d9c69bf80bc736b4ce100adc552cb5a04705
meganide <renashassan@hotmail.com>
2025-08-24 16:18:02 +0200

    fix: last seen ago

--- src/app/app/room/[inviteCode]/_utils/date.ts
+++ src/app/app/room/[inviteCode]/_utils/date.ts
@@ -1,6 +1,8 @@
 import { formatDistanceToNow } from "date-fns"
 
 export function getTimeAgo(timestamp: number): string {
-  const distance = formatDistanceToNow(new Date(timestamp), { addSuffix: true })
-  return `Last seen ${distance}`
+  const distance = formatDistanceToNow(new Date(timestamp), {
+    addSuffix: false,
+  })
+  return `Last seen ${distance} ago`
 }
