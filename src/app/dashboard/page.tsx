3858ad362047e22b81496c59048226f93cbaf3d4
meganide <renashassan@hotmail.com>
2025-08-25 12:13:46 +0200

    fix: eslint

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -22,8 +22,8 @@ export default function Page() {
           <div className="flex flex-1 items-center gap-2 px-3">
             <SidebarTrigger />
             <Separator
-              orientation="vertical"
               className="mr-2 data-[orientation=vertical]:h-4"
+              orientation="vertical"
             />
             <Breadcrumb>
               <BreadcrumbList>
