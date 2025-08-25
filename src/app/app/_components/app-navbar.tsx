637b06f51c048ca9c06a8c2f23c84d87fafa39ed
meganide <renashassan@hotmail.com>
2025-08-25 10:19:10 +0200

    feat: add logo

--- src/app/app/_components/app-navbar.tsx
+++ src/app/app/_components/app-navbar.tsx
@@ -1,5 +1,6 @@
 import { MessagesSquare } from "lucide-react"
 
+import { Logo } from "@/components/logo"
 import { Button } from "@/components/ui/button"
 
 import { NavUser } from "./nav-user"
@@ -17,7 +18,7 @@ export function AppNavbar({ setIsChatSidebarOpen }: AppNavbarProps) {
     <div className="sticky top-0 z-50 w-full bg-neutral-700 px-4">
       <nav className="container mx-auto flex h-16 items-center justify-between gap-4">
         <section className="flex items-center gap-4">
-          <span className="mr-12">Quiz Battle</span>
+          <Logo showTitle />
         </section>
         <section className="flex items-center gap-3">
           <NavUser />
